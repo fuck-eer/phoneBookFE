eb21f4e7a90471c0ee33b3960d72a00d597b44a8
fuck-eer <nishchaychauhan8@gmail.com>
2021-01-26 16:06:09 +0530

    login cards

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginpageComponent implements OnInit {
 
+  showwhat:string='login';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onswitch(){
+
   }
 
 }
